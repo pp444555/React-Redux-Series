318975e1cafc56560a1a21b3fab1bea242ed4301
prasadps2000 <patilprasad152000@gmail.com>
2025-08-29 22:18:19 +0530

    useState commit

--- todoie-app/src/App.js
+++ todoie-app/src/App.js
@@ -1,17 +1,22 @@
 import Header from './Components/Header';
 import ToDoItem from './Components/ToDoItems';
 import Button from './Components/button';
+import CounterComponenet from './Components/CounterComponent';
 import './style.css';
 
 const App = () => {
   return (
     <div className="todo-container">
-      <Header title="To-Do-App"/>
+      {/* <Header title="To-Do-App"/>
       <ToDoItem text = "Code" completed = {true} />
       <ToDoItem text="Eat"/>
       <ToDoItem text = "Sleep"/>
       <ToDoItem text = "Play"/>
-      <Button/>
+      <Button/> */}
+      <CounterComponenet/>
+      <CounterComponenet/>
+      <CounterComponenet/>
+      <CounterComponenet/>
     </div>
   )
 };
---
+++ todoie-app/src/Components/CounterComponent.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+const CounterComponenet = () =>{
+    const [count,setCount] = useState(0);
+    const [value,setValue] = useState("");
+
+    const checkNumber = (count)=>{
+        if(count % 2 == 0){
+            setValue('Even')
+        }
+        else{
+            setValue("Odd")
+        }
+
+    }
+
+
+    return(
+    <div>
+    <p>Count component - {count}</p>
+    <h6>value is {value}</h6>
+    <h2>Number is {count % 2 === 0 ? "even" : "odd"}</h2>
+    <button onClick = { ()=> checkNumber(count)}>Check Value</button>
+    <button onClick = { ()=> setCount(count+1)}>Increment</button>
+    <button onClick = { ()=> setCount(count-1)}>decrement</button>
+
+    </div>
+    )
+}
+
+export default CounterComponenet
